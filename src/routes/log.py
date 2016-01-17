8bedd096c68ed2ec473492ff49ace7eabd90c2d1
cquest <cquest@openstreetmap.fr>
2016-01-17 12:15:02 +0100

    API /log first version

---
+++ src/routes/log.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 16 janv. 2016
+
+@author: christian
+'''
+
+import falcon
+import os
+from PIL import Image
+from psycopg2.extensions import adapt
+
+import config
+from db import db
+
+
+class Log(object):
+    '''
+    Get logged data
+    '''
+
+    _table = "log"
+    
+    def on_get(self, req, resp):
+            '''Return logged requests
+            '''
+
+            cur = db.cursor()
+            
+            #id (uuid), path (str), geom (geom), address (str), level (str), building (str)
+            query = "SELECT array_to_json(array_agg(row_to_json(t)))::text FROM (select EXTRACT(epoch FROM age(time,now()))::integer as age, st_asgeojson(loc) as geometry from log where time > now()- interval '1 hour') as t;"
+            cur.execute(query)
+            log_data = cur.fetchone()[0]
+
+            resp.set_header('X-Powered-By', 'OpenEvacMap')
+            if log_data is None:
+                resp.status = falcon.HTTP_404
+            else:
+                resp.status = falcon.HTTP_200
+                resp.set_header('Access-Control-Allow-Origin', '*')
+                resp.set_header('Access-Control-Allow-Headers', 
+                                'X-Requested-With')
+                resp.body = (log_data)
+
+            cur.close()
+
